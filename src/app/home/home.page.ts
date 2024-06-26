f32252d6c4ca53e98503ffcd0e5584ca2cb5823c
sulalita kunin <sulalitakunin@gmail.com>
2024-06-26 14:03:02 +0700

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  weight ? : number ;
+  hight ? : number ;
+  result ? : number ;
+  result_text ? : string = "" ;
+
   constructor() {}
+
+  oncal() {
+   // console.log("น้ำหนัก = : " + this.weight);
+   // console.log("ส่วนสูง = : " + this.hight);
+    if(this.hight == undefined || this.weight == undefined ){
+      alert ("กรุณากรอกข้อมูลให้ครบ");
+      return;
+    }
+    var hi = this.hight / 100;
+    this.result = this.weight / (hi*hi);
+    // console.log("ผลลัพธ์ ="+this.result); //
+
+    if(this.result < 18.50){
+      this.result_text = "อยู่ในเกณฑ์น้ำหนักน้อย//ผอม";
+  }else if(this.result >=18.50 && this.result <= 22.90){
+     this.result_text = "อยู่ในเกณฑ์ปกติ";
+  }else if(this.result >= 23 && this.result <= 24.90){
+      this.result_text = "น้ำหนักเกิน";
+  }else if(this.result >= 25 && this.result <= 29.90){
+     this.result_text = "โรคอ้วนระยะที่ 1"
+  }else{
+    this.result_text = "โรคอ้วนระยะที่ 2"
+  }
+
+}
 
 }
